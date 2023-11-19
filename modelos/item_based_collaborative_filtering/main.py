6278c7104911201c1d3899a512a6221196e4bf65
Thales Cristian <thales.cristianegenio@gmail.com>
2023-11-19 16:18:40 -0300

    Ajustando imput

--- modelos/item_based_collaborative_filtering/main.py
+++ modelos/item_based_collaborative_filtering/main.py
@@ -1,11 +1,11 @@
 import dataset
 from sklearn.neighbors import NearestNeighbors
 
-title_input = input("TITLE: ")
-quantity = input("QUANTITY: ")
-quantity_input = int(quantity)
-
 df = dataset.import_data()
+
+title_input = input("TITLE (str): ")
+quantity = input("RECOMMENDATIONS TO GET (int): ")
+quantity_input = int(quantity)
 
 near = NearestNeighbors(metric='cosine', algorithm='brute')
 near.fit(df.values)
@@ -15,15 +15,13 @@ def recommend_movie_by_item(title):
     index_user_likes = df.index.tolist().index(title)
     sim_movies = indices[index_user_likes].tolist()
     movie_distances = distances[index_user_likes].tolist()
-
-    print('Similar Movies to ' + str(title) + ':\n')
-
+    print('==============================================')
+    print('MOVIES SIMILAR TO: ' + str(title) + ':\n')
     j = 1
 
     for i, distance in zip(sim_movies, movie_distances):
-        print(str(j) + ': ' + str(df.index[i]) + ', the distance with ' + str(title) + ': ' + str(distance))
+        print(str(j) + ': ' + str(df.index[i]) + ' | [Distance: ' + str(distance) + ' ]')
         j = j + 1
-
     print('\n')
 
 recommend_movie_by_item(title_input)
