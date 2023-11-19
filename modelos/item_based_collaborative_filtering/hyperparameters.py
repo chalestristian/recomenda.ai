fe6142e725ecaa4eae460682c1d27e89a55f18c7
Thales Cristian <thales.cristianegenio@gmail.com>
2023-11-19 18:31:30 -0300

    Limpando o código

---
+++ modelos/item_based_collaborative_filtering/hyperparameters.py
@@ -0,0 +1,33 @@
+# Hyperparameters
+
+def n_neighbors():
+    print('N_NEIGHBORS(int): from 2 up to the list length. (default: 3)')
+    quantity = input("n_neighbors: ")
+    n_neighbors_input = int(quantity)
+
+    # VALIDATION:
+    if n_neighbors_input < 2:
+        n_neighbors_input = 3
+
+    return n_neighbors_input
+
+
+def metric():
+    print('METRIC(str): cosine | euclidean | manhattan. (default: cosine)')
+    metric_input = input("metric: ").lower()
+
+    # VALIDATION:
+    if metric_input != "euclidean" and metric_input != "manhattan":
+        metric_input = "cosine"
+
+    return metric_input
+
+
+def algorithm():
+    print('ALGORITHM(str): brute | ball_tree | kd_tree | auto. (default: brute)')
+    algorithm_input = input("algorithm: ").lower()
+
+    if algorithm_input != "ball_tree" and algorithm_input != "kd_tree" and algorithm_input != "auto":
+        algorithm_input = "brute"
+
+    return algorithm_input
--- modelos/item_based_collaborative_filtering/main.py
+++ modelos/item_based_collaborative_filtering/main.py
@@ -1,61 +1,39 @@
 import dataset
+import hyperparameters as hp
 from sklearn.neighbors import NearestNeighbors
 
 df = dataset.import_data()
 
-# Hyperparameters
-print('========================')
+print('============================================================================\n')
 print('HYPERPARAMETERS OPTIONS: \n')
-
-print('N_NEIGHBORS(int): from 2 up to the list length. (default: 3)')
-quantity = input("n_neighbors: ")
-n_neighbors_input = int(quantity)
+n_neighbors = hp.n_neighbors()
 print('\n')
-
-print('METRIC(str): cosine | euclidean | manhattan. (default: cosine)')
-metric_input = input("metric: ")
+metric = hp.metric()
 print('\n')
+algorithm = hp.algorithm()
+print('\n')
+print('============================================================================\n')
+title_input = input("MOVIE TITLE (str): ")
 
-print('ALGORITHM(str): brute | ball_tree | kd_tree | auto. (default: brute)')
-algorithm_input = input("algorithm: ")
-
-metric_input = metric_input.lower()
-algorithm_input = algorithm_input.lower()
-
-print('========================\n')
-
-title_input = input("TITLE (str): ")
-
-# VALIDATION:
-if n_neighbors_input < 2:
-    n_neighbors_input = 3
-
-if metric_input != "euclidean" and metric_input != "manhattan":
-    metric_input = "cosine"
-
-if algorithm_input != "ball_tree" and algorithm_input != "kd_tree" and algorithm_input != "auto":
-    algorithm_input = "brute"
-
-near = NearestNeighbors(metric=metric_input, algorithm=algorithm_input)
+near = NearestNeighbors(metric=metric, algorithm=algorithm)
 near.fit(df.values)
-distances, indices = near.kneighbors(df.values, n_neighbors=n_neighbors_input)
+distances, indices = near.kneighbors(df.values, n_neighbors=n_neighbors)
 
 
 def recommend_movie_by_item(title):
     index_user_likes = df.index.tolist().index(title)
     sim_movies = indices[index_user_likes].tolist()
     movie_distances = distances[index_user_likes].tolist()
-    print('==============================================')
-    print('MOVIES SIMILAR TO [ ' + str(title) + " ] - "
-          "(n_neighbors: " + str(n_neighbors_input) +
-          " | metric: " + str(metric_input) +
-          " | algorithm: " + str(algorithm_input) + ' ) \n')
+    print("MOVIES SIMILAR TO [ " + str(title) + " ] - "
+          "(n_neighbors: " + str(n_neighbors) +
+          " | metric: " + str(metric) +
+          " | algorithm: " + str(algorithm) + " ) \n"
+          )
     j = 1
 
     for i, distance in zip(sim_movies, movie_distances):
         print(str(j) + ': ' + str(df.index[i]) + ' | [Distance: ' + str(distance) + ' ]')
         j = j + 1
-    print('\n')
 
 
 recommend_movie_by_item(title_input)
